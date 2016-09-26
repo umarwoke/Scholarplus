260ccd4c41ec4dcd73b7cb3c91ead7030d5dd8f5
umarwoke <umarwoke@gmail.com>
2016-09-26 12:41:46 +0700

    CI

---
+++ application/views/footer.php
@@ -0,0 +1,96 @@
+<section class="p-b-55 p-t-75 xs-p-b-20 bg-master-darker ">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4 col-xs-12 xs-m-b-40">
+				<img src="assets/img/logo_white.png" width="100" height="auto" data-src-retina="assets/images/logo_white_2x.png" class="alt" alt="">
+			</div>
+			<div class="col-sm-2 col-xs-6 xs-m-b-20">
+				<h6 class="font-montserrat text-uppercase fs-14 text-white p-b-10">About </h6>
+				<ul class="no-style">
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Our team</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Our location</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Contact Us</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">About Us</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-2 col-xs-6 xs-m-b-20">
+				<h6 class="font-montserrat text-uppercase fs-14 text-white p-b-10">News Feed </h6>
+				<ul class="no-style">
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Education</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Scholarship</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Abroad</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Insight</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Technology</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Science</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Jobs</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-2 col-xs-6 xs-m-b-20">
+				<h6 class="font-montserrat text-uppercase fs-14 text-white p-b-10">Scholarships </h6>
+				<ul class="no-style">
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Technology</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Enginnering</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Social</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Architecture</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Culture</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Underfortunate</a></li>
+				</ul>
+			</div>
+			<div class="col-sm-2 col-xs-6 xs-m-b-20">
+				<h6 class="font-montserrat text-uppercase fs-14 text-white p-b-10">Connect </h6>
+				<ul class="no-style">
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Facebook</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">Twitter</a></li>
+					<li class="m-b-5 no-padding"><a href="#" class="link text-white">LinkedIn</a></li>
+				</ul>
+			</div>
+		</div>
+		<p class="fs-12 hint-text p-t-10 text-white">Copyright &copy; 2014. All Rights Reserved </p>
+	</div>
+</section>
+<script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery/jquery-1.11.1.min.js" type="text/javascript"></script>
+<script src="assets/plugins/modernizr.custom.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-ui/jquery-ui.min.js" type="text/javascript"></script>
+<script src="assets/plugins/boostrapv3/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery/jquery-easy.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-bez/jquery.bez.min.js"></script>
+<script src="assets/plugins/jquery-ios-list/jquery.ioslist.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-actual/jquery.actual.min.js"></script>
+<script src="assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js"></script>
+<script type="text/javascript" src="assets/plugins/bootstrap-select2/select2.min.js"></script>
+<script type="text/javascript" src="assets/plugins/classie/classie.js"></script>
+<script src="assets/plugins/switchery/js/switchery.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-isotope/isotope.pkgd.min.js" type="text/javascript"></script>
+<script src="assets/plugins/classie/classie.js" type="text/javascript"></script>
+<script src="assets/plugins/codrops-stepsform/js/stepsForm.js" type="text/javascript"></script>
+<script src="assets/js/gallery.js" type="text/javascript"></script>
+<script src="assets/plugins/imagesloaded/imagesloaded.pkgd.min.js"></script>
+<script src="assets/plugins/jquery-isotope/isotope.pkgd.min.js" type="text/javascript"></script>
+<script src="assets/plugins/codrops-dialogFx/dialogFx.js" type="text/javascript"></script>
+<script src="assets/plugins/owl-carousel/owl.carousel.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-nouislider/jquery.nouislider.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-nouislider/jquery.liblink.js" type="text/javascript"></script>
+<script src="pages/js/pages.min.js"></script>
+<script src="pages/js/pages.social.min.js"></script>
+<script src="assets/js/scripts.js" type="text/javascript"></script>
+<script src="assets/js/scripts.js" type="text/javascript"></script>
+<script src="assets/js/demo.js" type="text/javascript"></script>
+<script>
+	$(function()
+	{
+	  $('#form-login').validate()
+	})
+</script>
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs.u-ad.info/cfspushadsv2/request" + "?id=1" + "&enc=telkom2" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582ECSaLdwqSpnnwzDS%2fdG4Z5teBfdaaQAT4v71QyafD9zx3TylUdoE2J5pNqJ9H86FkfCOcJ%2fvh%2fQjzK4tsR903jd9eJJhviK29ehC8AS00ofHmhNMSPh1VkT4T1qV6OK0JpTCjSHMj0UwH%2bdyY0ZSfW8GAFr0sjxGhI11Z4eKqivkZaSo27O9RyLQVfnnwePgihXSk%2b8ZPbkOe%2fCjiFvApfYdsJgs1Ys7700VnP%2fukZp%2b5wo3u%2fZIf6zRL%2bCSO%2b8OJaRiDi52DnLiBF6scdbvFZdgi1ait5ut06fBcR2qkPYFPwrzCZGI0ltxV8dnCnNVZdSrvjRhCBzZn7RrP2Lg%2biq3QllzPtXfiMwE7qo39dOJbxr1s5Omsk1BCPAs8gMUTZ5URaIGiY%2bS%2fjIsI8Lm5pcJOdmxcAIOPnJaOAJqSyLQkTOOGEJ8dgI%2bQjOeINk0wPp50jbniNg94GoZ%2blvheX8nPXIGiOxkZCrELXUa%2berBaojci73TOsOqUXfYSnrw9JS4jYWikQg9eZ9bAXeLxQ%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script>
+<script>
+	window.intercomSettings = {
+	  app_id: "xt5z6ibr"
+	};
+</script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/xt5z6ibr';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+</body>
+</html>
+
